ffa206da5c28e01f0d3557b293855969222e2f9d
Gareth Murphy <garethcmurphy@gmail.com>
2019-07-26 16:02:31 +0200

    add reader script

---
+++ x.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import h5py
+f = h5py.File('FMC-PICO-evaluation.hdf5', 'r')
+print(list(f.keys()))
+g=f['/FMC-PICO/FMC-Pico-1m4-C3'].keys()
+print(list(g))
+a=f['/FMC-PICO/FMC-Pico-1m4-C3/18002_CH4_RNG1_OC'].attrs
+for name in a:
+    print(name, a[name])
+f.close()
+
