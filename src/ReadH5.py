b8da1c51b817aa6257e2da7af8aed2bb4a5831b0
Gareth Murphy <garethcmurphy@gmail.com>
2019-07-29 09:45:08 +0200

    remove coments

--- src/ReadH5.py
+++ src/ReadH5.py
@@ -18,11 +18,6 @@ class ReadH5:
                 print(a)
                 for name in a:
                     print("{" ,name, ":" ,a[name] , "}")
-            # g=f['/FMC-PICO/FMC-Pico-1m4-C3'].keys()
-            # print(list(g))
-            # a=f['/FMC-PICO/FMC-Pico-1m4-C3/18002_CH4_RNG1_OC'].attrs
-            # for name in a:
-            #    print(name, a[name])
             f.close()
 
 
